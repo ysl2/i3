ba452f5db7528db7ed0144d6e9bf677b1836bb47
ysl2 <www.songli.yu@gmail.com>
2023-11-12 01:05:22 +0800

    Add autostart.sh

---
+++ autostart.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+picom -b
+
+~/.dwm/dwm-natural-scrolling.sh
+
+# single_instance() {
+#     result=$(ps aux | grep -v grep | grep "$1")
+#     if [ "$result" != '' ]; then
+#         killall "$1"
+#     fi
+#     "$1" &
+# }
+#
+# single_instance slstatus
+
+[ -f ~/.dwm/autostart.localhost.sh ] && ~/.dwm/autostart.localhost.sh
