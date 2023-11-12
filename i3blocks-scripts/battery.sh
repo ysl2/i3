a0040d52845fdb7a162755f06e824e5d5814773e
ysl2 <www.songli.yu@gmail.com>
2023-11-12 13:45:53 +0800

    Unuse i3blocks-contrib. Instead use myself command.

---
+++ i3blocks-scripts/battery.sh
@@ -0,0 +1,28 @@
+get_battery() {
+    status_file=/sys/class/power_supply/BAT0/status
+    capacity_file=/sys/class/power_supply/BAT0/capacity
+    if [ ! -e "$status_file" ] || [ ! -e "$capacity_file" ]; then
+        echo ''
+        return
+    fi
+
+    status=$(cat "$status_file")
+    if [ "$status" = "Charging" ]; then
+        battery_icon=""
+    elif [ "$status" = "Discharging" ]; then
+        battery_icon=""
+    else
+        battery_icon=""
+    fi
+
+    capacity=$(cat "$capacity_file")
+    if [ -z "$capacity" ]; then
+        echo ''
+        return
+    fi
+
+    # printf -v capacity "%3s" "$capacity"
+    echo "$battery_icon $capacity%"
+}
+
+get_battery
