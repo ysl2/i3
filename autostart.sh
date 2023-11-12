116a50a16a17166894ee78c4315376b53278c5b4
ysl2 <www.songli.yu@gmail.com>
2023-11-12 08:56:06 +0800

    Add i3 autostart scripts compatible with common wm scripts.

--- autostart.sh
+++ autostart.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
-picom -b
 
-~/.dwm/dwm-natural-scrolling.sh
+# Common
+~/.wm/autostart.sh # -> picom, natural scrolling, keyboard, etc.
 
-# single_instance() {
-#     result=$(ps aux | grep -v grep | grep "$1")
-#     if [ "$result" != '' ]; then
-#         killall "$1"
-#     fi
-#     "$1" &
-# }
-#
-# single_instance slstatus
+# I3 spec.
+autotiling &
 
-[ -f ~/.dwm/autostart.localhost.sh ] && ~/.dwm/autostart.localhost.sh
+# Localhost config
+[ -f ~/.config/i3/autostart.localhost.sh ] && ~/.config/i3/autostart.localhost.sh
