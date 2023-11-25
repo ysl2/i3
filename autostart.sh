9919d5b4993e218395873ca55ae09a4ad9a8dab8
ysl2 <www.songli.yu@gmail.com>
2023-11-25 21:57:20 +0800

    Enhance sticky and float.
    Enhance float for scratchpad

--- autostart.sh
+++ autostart.sh
@@ -6,6 +6,7 @@
 # I3 spec.
 autotiling &
 ~/.config/i3/i3-scripts/swallow.py &
+~/.config/i3/i3-scripts/sticky.py &
 
 # Localhost config
 [ -f ~/.config/i3/autostart.localhost.sh ] && ~/.config/i3/autostart.localhost.sh
---
+++ i3-scripts/sticky.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import i3ipc
+
+i3 = i3ipc.Connection()
+
+
+def on_workspace_focus(change, old):
+    focused = i3.get_tree().find_focused()
+
+    if focused and focused.sticky:
+        i3.command('focus mode_toggle')
+
+
+if __name__ == '__main__':
+    i3.on('workspace::focus', on_workspace_focus)
+    i3.main()
