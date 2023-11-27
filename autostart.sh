61120d64fe493f6c2ca201d417a5bc7f998f88b6
ysl2 <www.songli.yu@gmail.com>
2023-11-27 22:27:38 +0800

    Add sticky_toggle.py
    Remove whitespace line.

--- autostart.sh
+++ autostart.sh
@@ -6,7 +6,7 @@
 # I3 spec.
 autotiling &
 ~/.config/i3/i3-scripts/swallow.py &
-~/.config/i3/i3-scripts/sticky.py &
+~/.config/i3/i3-scripts/sticky_nofocus.py &
 
 # Localhost config
 [ -f ~/.config/i3/autostart.localhost.sh ] && ~/.config/i3/autostart.localhost.sh
---
+++ i3-scripts/sticky_toggle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+import i3ipc
+
+
+def main():
+    i3 = i3ipc.Connection()
+    focused = i3.get_tree().find_focused()
+
+    if focused.sticky:
+        focused.command('sticky disable')
+        return
+    if 'off' in focused.floating:
+        focused.command('floating enable')
+        focused.command('resize set 50 ppt 50 ppt')
+        focused.command('move position center')
+    focused.command('sticky enable')
+
+
+if __name__ == '__main__':
+    main()
