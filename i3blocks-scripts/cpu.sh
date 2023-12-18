b9e8b20fe814ada89afe74a4f5181788bc7abb9f
ysl2 <www.songli.yu@gmail.com>
2023-12-18 13:19:24 +0800

    Change default terminal to alacritty; Update cpu.sh to be more accurate.
    Add comment

--- i3blocks-scripts/cpu.sh
+++ i3blocks-scripts/cpu.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-echo "Cpu:$(top -bn1 | grep "%Cpu(s)" | awk '{print $2}' | cut -d "." -f 1)% "
+
+# ref: https://stackoverflow.com/questions/9229333/how-to-get-overall-cpu-usage-e-g-57-on-linux
+cpu=$(awk '{u=$2+$4; t=$2+$4+$5; if (NR==1){u1=u; t1=t;} else printf "%.1f", ($2+$4-u1) * 100 / (t-t1) "%"; }' <(grep 'cpu ' /proc/stat) <(sleep .1;grep 'cpu ' /proc/stat))
+echo "Cpu:${cpu}% "
