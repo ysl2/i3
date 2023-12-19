f74e69fea9277500e874eac95c1e0bbd9a17b164
ysl2 <www.songli.yu@gmail.com>
2023-12-19 11:34:58 +0800

    autotiling -> i3-autolayout
    autotiling -> i3-layouts
    
    Update README.md

--- autostart.sh
+++ autostart.sh
@@ -1,13 +1,21 @@
 #!/bin/sh
 
-# Common
+# ==============
+# === Common ===
+# ==============
 # -> picom, natural scrolling, keyboard, etc.
 [ -f ~/.scripts/window-manager/autostart.sh ] && ~/.scripts/window-manager/autostart.sh
 
-# I3 spec.
-autotiling &
-~/.config/i3/i3-scripts/swallow.py &
-~/.config/i3/i3-scripts/sticky_nofocus.py &
 
-# Localhost config
+# ===============
+# === I3 spec ===
+# ===============
+i3-autolayout autolayout &
+~/.config/i3/autostart/swallow.py &
+~/.config/i3/autostart/sticky_nofocus.py &
+
+
+# ========================
+# === Localhost config ===
+# ========================
 [ -f ~/.config/i3/autostart.localhost.sh ] && ~/.config/i3/autostart.localhost.sh
