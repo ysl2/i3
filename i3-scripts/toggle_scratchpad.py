121d32a457e8b29607d58d7aa70c9fd346d09251
ysl2 <www.songli.yu@gmail.com>
2023-12-31 22:46:02 +0800

    Fix toggle_scratchpad.py for fullscreen.

--- i3-scripts/toggle_scratchpad.py
+++ i3-scripts/toggle_scratchpad.py
@@ -4,15 +4,22 @@ import i3ipc
 import argparse
 
 
-def show_spterm(spterm):
+parser = argparse.ArgumentParser()
+parser.add_argument('scratchpad_class', type=str, nargs='?', default='spterm')
+args = parser.parse_args()
+
+
+def show_spterm(ipc, spterm):
+    focused = ipc.get_tree().find_focused()
+    if focused.fullscreen_mode:
+        focused.command('fullscreen disable')
+    if not spterm:
+        ipc.command(f'exec --no-startup-id alacritty -c {args.scratchpad_class}')
+        return
     spterm.command('scratchpad show, resize set 50 ppt 50 ppt, move position center')
 
 
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('scratchpad_class', type=str, nargs='?', default='spterm')
-    args = parser.parse_args()
-
     ipc = i3ipc.Connection()
 
     # If we get spterm in scratchpad, we should show it.
@@ -22,7 +29,7 @@ def main():
         .find_classed(args.scratchpad_class)
     ):
         spterm = spterm[0]
-        show_spterm(spterm)
+        show_spterm(ipc, spterm)
         return
     # If we get spterm in outside windows, then:
     # Case 1: If in current workspace, we should hide it.
@@ -33,10 +40,10 @@ def main():
         spterm.command('move scratchpad')
         # If spterm not in current workspace, we should show it to current workspace.
         if spterm.workspace().name != ipc.get_tree().find_focused().workspace().name:
-            show_spterm(spterm)
+            show_spterm(ipc, spterm)
         return
     # If we cannot find spterm, we should open it.
-    ipc.command(f'exec --no-startup-id alacritty -c {args.scratchpad_class}')
+    show_spterm(ipc, None)
 
 
 if __name__ == '__main__':
