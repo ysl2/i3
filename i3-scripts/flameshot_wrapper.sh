1cf806668e3fd4aa35dd9f69cb997d58771b720f
ysl2 <www.songli.yu@gmail.com>
2024-01-11 16:01:25 +0800

    Update flameshot_wrapper.sh

--- i3-scripts/flameshot_wrapper.sh
+++ i3-scripts/flameshot_wrapper.sh
@@ -2,12 +2,15 @@
 
 focusedwindow_before=$(xdotool getactivewindow)
 
-if [ -z "$1" ]; then
-    flameshot gui
+flameshot_option=$1
+tesseract_option=$2
+
+if [ -z "$tesseract_option" ]; then
+    flameshot "$flameshot_option"
 else
     TESSADTA_PREFIX=~/.vocal/tessdata_best \
-    flameshot gui --raw \
-        | tesseract stdin stdout -l "$1" \
+    flameshot "$flameshot_option" --raw \
+        | tesseract stdin stdout -l "$tesseract_option" \
         | xclip -in -selection clipboard
 fi
 
