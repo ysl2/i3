a1f6f6474689c0d9072347b85b2f4fcf8f70b07d
ysl2 <www.songli.yu@gmail.com>
2024-03-09 22:13:43 +0800

    Refactor keymap sequence; Add rename title format and bind it to keymap.
    Add $mod+Shift+n and $mod+Shift+p to move container to next and prev workspace.

--- i3-scripts/mv_con_to_ws.py
+++ i3-scripts/mv_con_to_ws.py
@@ -2,16 +2,28 @@
 
 import i3ipc
 import subprocess
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-t', '--title', action='store_true')
+args = parser.parse_args()
 
 ipc = i3ipc.Connection()
 
 
 def main():
     focused = ipc.get_tree().find_focused()
-    result = subprocess.run("zenity --entry --title '' --text 'Move current container to workspace:'", shell=True, capture_output=True, text=True)
+    if args.title:
+        prompt = 'Rename current container title to:'
+    else:
+        prompt = 'Move current container to workspace:'
+    result = subprocess.run(f"zenity --entry --title '' --text '{prompt}'", shell=True, capture_output=True, text=True)
     result = result.stdout.strip()
-    focused.command(f'move container to workspace "{result}"')
-    ipc.command(f'workspace "{result}"')
+    if args.title:
+        focused.command(f'title_format "{result}"')
+    else:
+        focused.command(f'move container to workspace "{result}"')
+        ipc.command(f'workspace "{result}"')
 
 
 if __name__ == '__main__':
